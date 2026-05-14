2f1a8222cad7fd3c2cb96a207cd9dbdf808b2ce1
ZamestoTV <34053496+Hubbotu@users.noreply.github.com>
2026-05-14 07:36:58 +0300

    Create ruRU.lua

---
+++ Locales/ruRU.lua
@@ -0,0 +1,45 @@
+--[[
+    VeevHUD - Russian Localization ZamestoTV
+]]
+
+local _, addon = ...
+
+addon.L = addon.L or {}
+local L = addon.L
+
+-- General
+L["VeevHUD"] = "VeevHUD"
+L["Enabled"] = "Включено"
+L["Disabled"] = "Отключено"
+L["Lock"] = "Закрепить"
+L["Unlock"] = "Разблокировать"
+L["Reset"] = "Сброс"
+L["Scale"] = "Масштаб"
+L["Alpha"] = "Прозрачность"
+L["Width"] = "Ширина"
+L["Height"] = "Высота"
+
+-- Modules
+L["Resource Bar"] = "Панель ресурса"
+L["Health Bar"] = "Панель здоровья"
+L["Cooldown Icons"] = "Иконки способностей"
+L["Cast Bar"] = "Полоса применения"
+L["Proc Glow"] = "Подсветка проков"
+
+-- Settings
+L["Show when"] = "Показывать, когда"
+L["Always"] = "Всегда"
+L["In Combat"] = "В бою"
+L["Has Target"] = "Есть цель"
+L["In Instance"] = "В подземелье"
+
+-- Spell categories / Row names
+L["Primary Row"] = "Основной ряд"
+L["Secondary Row"] = "Второй ряд"
+L["Core Rotation"] = "Основная ротация"  -- Legacy
+L["Offensive Cooldowns"] = "Атакующие способности"
+L["Defensive Cooldowns"] = "Защитные способности"
+L["Interrupts"] = "Прерывания"
+L["Crowd Control"] = "Контроль"
+L["Movement"] = "Передвижение"
+L["Utility"] = "Утилитарные способности"
